6c41a6647446af778f4fbe76791e684e30621d13
ComputerScientist-01 <rathi406@gmail.com>
2021-11-07 10:47:04 +0530

    ✏️ added social links

--- src/Components/SideLinks/SideLinks.js
+++ src/Components/SideLinks/SideLinks.js
@@ -1,20 +1,38 @@
-import './sidelinks.scss'
-import {LinkLogoFacebook, LinkLogoInstagram, LinkLogoLinkedin, LinkLogoMedium, LinkLogoTwitter, LinkLogoYoutube, LinkLine} from '../../Images'
+import "./sidelinks.scss";
+import {
+	LinkLogoFacebook,
+	LinkLogoInstagram,
+	LinkLogoLinkedin,
+	LinkLogoMedium,
+	LinkLogoTwitter,
+	LinkLogoYoutube,
+	LinkLine,
+} from "../../Images";
 
+const SideLinks = ({ fb, insta, linkedIn, medium, twitter, utube }) => {
+	return (
+		<div className="sidelinks_container">
+			<a className="sidelinks_item" href={fb}>
+				<LinkLogoFacebook />
+			</a>
+			<a className="sidelinks_item" href={insta}>
+				<LinkLogoInstagram />
+			</a>
+			<a className="sidelinks_item" href={linkedIn}>
+				<LinkLogoLinkedin />
+			</a>
+			<a className="sidelinks_item" href={medium}>
+				<LinkLogoMedium />
+			</a>
+			<a className="sidelinks_item" href={twitter}>
+				<LinkLogoTwitter />
+			</a>
+			<a className="sidelinks_item" href={utube}>
+				<LinkLogoYoutube />
+			</a>
+			<LinkLine />
+		</div>
+	);
+};
 
-const SideLinks = ({fb, insta, linkedIn, medium, twitter, utube}) => {
-  return (
-    <div className='sidelinks_container'>
-        <a className='sidelinks_item' href={fb}><LinkLogoFacebook/></a>
-        <a className='sidelinks_item' href={insta}><LinkLogoInstagram/></a>
-        <a className='sidelinks_item' href={linkedIn}><LinkLogoLinkedin/></a>
-        <a className='sidelinks_item' href={medium}><LinkLogoMedium/></a>
-        <a className='sidelinks_item' href={twitter}><LinkLogoTwitter/></a>
-        <a className='sidelinks_item' href={utube}><LinkLogoYoutube/></a>
-        <LinkLine/>
-    </div>
-  )
-}
-
-
-export default SideLinks
+export default SideLinks;
