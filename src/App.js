056c7b9cd4716aff743465228bb9423534c78b5c
ComputerScientist-01 <rathi406@gmail.com>
2021-11-07 10:53:26 +0530

    ✏️ added social links

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import {HashRouter, Switch, Route, Redirect} from 'react-router-dom';
-import {Home, Projects, Initiatives} from './Pages'
-import {Navbar, SideLinks, Footer} from './Components'
-import './App.css'
+import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
+import { Home, Projects, Initiatives } from "./Pages";
+import { Navbar, SideLinks, Footer } from "./Components";
+import "./App.css";
 
 function App() {
-  return (
-    <HashRouter>
-      <div className="app">
-        <Navbar/>
-        <Switch>
-          <Redirect from='/' to='/home' exact/>
-          <Route exact path='/home' component={Home} />
-          <Route exact path='/projects' component={Projects} />
-          <Route exact path='/initiatives' component={Initiatives}/>
-        </Switch>
-        <Footer/>
-        <SideLinks fb='#' insta='#' linkedIn='#' medium='#' twitter='#' utube='#' />
-      </div>
-    </HashRouter>
-  )
+	return (
+		<HashRouter>
+			<div className="app">
+				<Navbar />
+				<Switch>
+					<Redirect from="/" to="/home" exact />
+					<Route exact path="/home" component={Home} />
+					<Route exact path="/projects" component={Projects} />
+					<Route exact path="/initiatives" component={Initiatives} />
+				</Switch>
+				<Footer />
+				<SideLinks
+					fb="https://www.facebook.com/rathijayvardhan/"
+					insta="https://www.instagram.com/jayvardhan.rathi/"
+					linkedIn="https://www.linkedin.com/in/rathi406/"
+					medium="https://medium.com/@ourmine"
+					twitter="https://twitter.com/ml__engineer"
+					utube="https://www.youtube.com/channel/UCjFDgCGGoLDsCXKrwdTDZ-g"
+				/>
+			</div>
+		</HashRouter>
+	);
 }
 
 export default App;
